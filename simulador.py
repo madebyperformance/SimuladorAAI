cfc18b7817ae4d796146d95b68dba8fd18cd9967
madebyperformance <124921985+madebyperformance@users.noreply.github.com>
2023-05-09 18:01:18 -0300

    Update simulador.py

--- simulador.py
+++ simulador.py
@@ -5,7 +5,8 @@ import numpy as np
 st.set_page_config(
     page_title='Simulador Partnership Assessor',
     page_icon=':bar_chart:',
-    layout='wide'
+    layout='wide',
+    theme='light'
 )
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
